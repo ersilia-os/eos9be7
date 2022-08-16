57f303db59fb6dba0be8ae48a3a51c5d9e963f10
brosular <brosular@stanford.edu>
2022-08-16 10:04:36 -0400

    Update main.py

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -1,40 +1,35 @@
-# imports
-import os
-import csv
-import joblib
 import sys
-from rdkit import Chem
-from rdkit.Chem.Descriptors import MolWt
+import csv
+import json
+import fcd
 
-# parse arguments
 input_file = sys.argv[1]
 output_file = sys.argv[2]
+smiles_list1_orig = []
+smiles_list2_orig = []
 
-# current file directory
-root = os.path.dirname(os.path.abspath(__file__))
-
-# checkpoints directory
-checkpoints_dir = os.path.abspath(os.path.join(root, "..", "..", "checkpoints"))
-
-# read checkpoints (here, simply an integer number: 42)
-ckpt = joblib.load(os.path.join(checkpoints_dir, "checkpoints.joblib"))
-
-# model to be run (here, calculate the Molecular Weight and add ckpt (42) to it)
-def my_model(smiles_list, ckpt):
-    return [MolWt(Chem.MolFromSmiles(smi))+ckpt for smi in smiles_list]
-    
-# read SMILES from .csv file, assuming one column with header
 with open(input_file, "r") as f:
     reader = csv.reader(f)
-    next(reader) # skip header
-    smiles_list = [r[0] for r in reader]
-    
-# run model
-outputs = my_model(smiles_list, ckpt)
+    next(reader)
+    for row in reader:
+        smiles_list1_orig.append(row[0])
+        smiles_list2_orig.append(row[1])
+        
+#Filter empty strings
+smiles_list1 = [smi for smi in smiles_list1_orig if smi]
+smiles_list2 = [smi for smi in smiles_list2_orig if smi]
 
-# write output in a .csv file
-with open(output_file, "w") as f:
+#Get canonical smiles and filter invalid ones
+can_smiles1 = [smi for smi in fcd.canonical_smiles(smiles_list1) if ((smi is not None))]
+can_smiles2 = [smi for smi in fcd.canonical_smiles(smiles_list2) if ((smi is not None))]
+
+#Make prediction using default ChemNet model
+print(can_smiles1)
+print(can_smiles2)
+fcd_score = fcd.get_fcd(can_smiles1, can_smiles2)
+
+#Write output
+with open(output_file, 'w') as f:
     writer = csv.writer(f)
-    writer.writerow(["value"]) # header
-    for o in outputs:
-        writer.writerow([o])
+    writer.writerow(["fcd_score"]) # header
+    writer.writerow([fcd_score])
